d53aa7ff34dad6133394fcbb57d9f7e797d14807
DY <yershov@ptmk.ru>
2022-01-11 13:47:46 +0300

    Init

---
+++ Attachment/JSControl/Ctrl.CytoscapeJS/base.js
@@ -0,0 +1,27 @@
+﻿/*!
+ * JS Control interop for TurboX ver. 10.5
+ */
+
+if (window.location.hash == '#client'){
+  window.callProcEx = function(eventName,eventParams){
+    window.open('turbo://passEvent?Event=' + eventName + '&Params=' + JSON.stringify(eventParams),'_self');
+  }
+
+  window.getData = function(){};
+}else if (window.location.hash == '#web'){
+  window.callProcEx = function(eventName,eventParams){
+    window.parent.postMessage({ dataset: {Event:eventName,Params:JSON.stringify(eventParams)}}, '*');
+  }
+}else{
+  window.callProcEx = function(eventName,eventParams){
+    console.log('EventName:' + eventName + ';EventParams:' + JSON.stringify(eventParams));
+  }
+  window.getData = function(){};
+}
+
+window.onload = function(){window.callProcEx("DocumentComplete");};
+
+window.execScriptAsync = function(key,script){
+  window.callProcEx('DoExecuteScriptAsyncResult',[key,window.eval(script)]);
+}
+
